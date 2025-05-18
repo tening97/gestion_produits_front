beb37a91228247754ecfa7c37381b38ddd5428d4
teningngom <71282930+tening97@users.noreply.github.com>
2025-05-18 00:03:35 +0000

    Delete product_backend-main directory

--- product_backend-main/src/main/java/org/example/service/RessourceService.java
+++
@@ -1,9 +0,0 @@
-package org.example.service;
-
-import org.example.entity.Ressource;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-public interface RessourceService {
-    Ressource uploadFile(MultipartFile file) throws IOException;
-}
--- product_backend-main/src/main/java/org/example/utils/Constants.java
+++
@@ -1,5 +0,0 @@
-package org.example.utils;
-
-public interface Constants {
-    String APP_ROOT = "api/v1";
-}
